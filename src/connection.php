fe5c6c1d741c70297e56aa34a6df1fef96d0b7cf
yindaheng98 <yindaheng98@163.com>
2020-07-29 14:07:53 +0800

    数据库连接修改

--- src/connection.php
+++ src/connection.php
@@ -1,13 +1,13 @@
 <?php
-$con = mysqli_connect('localhost:3306', 'CalendarsDB', 'CalendarsDB', 'CalendarsDB');
-$dbc = mysqli_connect('localhost:3306', 'CalendarsDB', 'CalendarsDB', 'CalendarsDB');
-if ($con->connect_errno)
+$con = mysqli_connect('mysql:3306', 'CalendarsDB', 'CalendarsDB', 'CalendarsDB');
+$dbc = mysqli_connect('mysql:3306', 'CalendarsDB', 'CalendarsDB', 'CalendarsDB');
+if (!mysqli_set_charset($con, "utf8")||$con->connect_errno)
 {
     die("连接失败: (" . $con->connect_errno . ") " . $con->connect_error);
 }
-if ($dbc->connect_errno)
+if (!mysqli_set_charset($dbc, "utf8")||$dbc->connect_errno)
 {
-    die("连接失败: (" . $con->connect_errno . ") " . $con->connect_error);
+    die("连接失败: (" . $dbc->connect_errno . ") " . $dbc->connect_error);
 }
 $redis=new Redis();
 $redis->connect('redis',6379);
